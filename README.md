74d5dae49aa1aaac46273f10b9f827849b2a3aeb
Christian Lück <christian@lueck.tv>
2014-08-21 00:44:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# clue/json-query [![Build Status](https://travis-ci.org/clue/php-json-query.svg?branch=master)](https://travis-ci.org/clue/php-json-query)
+
+An implementation of the *experimental* [JSON Query Language](https://github.com/clue/json-query-language) in PHP.
+
+> Note: This project is in early alpha stage! Feel free to report any issues you encounter.
+
+## Quickstart example
+
+Once [installed](#install), you can use the following code to get started:
+
+```php
+$matcher = new Matcher(array('age' => 20));
+
+$matcher->match(array('name' => 'Tester', 'age' => 20))); // true
+$matcher->match(array('name' => 'Tester', 'age' => 22))); // false
+```
+
+See also the [examples](examples).
+
+## Install
+
+The recommended way to install this library is [through composer](http://getcomposer.org). [New to composer?](http://getcomposer.org/doc/00-intro.md)
+
+```JSON
+{
+    "require": {
+        "clue/json-query": "dev-master"
+    }
+}
+```
+
+## License
+
+MIT
---
+++ src/Matcher.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Clue\JsonQuery;
+
+use DomainException;
+
+class Matcher
+{
+    private $filterExpression;
+    private $selectorSeparator = '.';
+
+    public function __construct($filterExpression)
+    {
+        $this->filterExpression = $filterExpression;
+    }
+
+    public function match($data)
+    {
+        return $this->matchFilter($data, $this->filterExpression);
+    }
+
+    private function matchFilter($data, $filter)
+    {
+        if ($this->isObject($filter)) {
+            return $this->matchAnd($data, $filter);
+        } elseif (is_bool($filter)) {
+            return $filter;
+        } else {
+            throw new DomainException('Invalid filter type');
+        }
+    }
+
+    private function matchOr($data, $filter)
+    {
+        if ($this->isVector($filter)) {
+            if (!$filter) {
+                return true;
+            }
+            foreach ($filter as $element) {
+                if ($this->matchFilter($data, $element)) {
+                    return true;
+                }
+            }
+        } elseif ($this->isObject($filter)) {
+            foreach ($filter as $key => $value) {
+                if ($this->matchValue($data, $key, $value)) {
+                    return true;
+                }
+            }
+        } else {
+            throw new DomainException('Invalid data type for $or combinator');
+        }
+
+        return false;
+    }
+
+    private function matchAnd($data, $filter)
+    {
+        if ($this->isVector($filter)) {
+            foreach ($filter as $element) {
+                if (!$this->matchFilter($data, $element)) {
+                    return false;
+                }
+            }
+        } elseif ($this->isObject($filter)) {
+            foreach ($filter as $key => $value) {
+                if (!$this->matchValue($data, $key, $value)) {
+                    return false;
+                }
+            }
+        } else {
+            throw new DomainException('Invalid data type for $and combinator');
+        }
+
+        return true;
+    }
+
+    private function matchValue($data, $column, $expectation)
+    {
+        if ($column === '$and') {
+            return $this->matchAnd($data, $expectation);
+        } elseif ($column === '$or') {
+            return $this->matchOr($data, $expectation);
+        } elseif ($column === '$not') {
+            return !$this->matchAnd($data, $expectation);
+        } elseif ($column[0] === '!') {
+            return !$this->matchValue($data, substr($column, 1), $expectation);
+        } elseif ($column[0] === '$') {
+            throw new DomainException('Unknown combinator "' . $column . '"');
+        }
+
+        $expectedValue = $expectation;
+        $comparator = '$is';
+
+        if (is_array($expectedValue)) {
+            if ($this->isVector($expectedValue)) {
+                // list of possible values
+                $comparator = '$in';
+            } else {
+                // custom comparator ('>', '<', ...)
+                $comparator = key($expectedValue);
+                $expectedValue = reset($expectedValue);
+
+                if ($comparator === '$not') {
+                    $comparator = ($this->isVector($expectedValue)) ? '!$in' : '!$is';
+                }
+
+                if ($comparator[0] === '!') {
+                    $comparator = substr($comparator, 1);
+                    return !$this->matchValue($data, $column, array($comparator => $expectedValue));
+                }
+            }
+        }
+
+        $actualValue = $this->fetchValue($data, $column);
+
+        if ($comparator === '$is') {
+            return ($actualValue === $expectedValue);
+        } elseif ($comparator === '$in') {
+            return in_array($actualValue, $expectedValue, true);
+        } elseif ($comparator === '$lt') {
+            return ($actualValue < $expectedValue);
+        } elseif ($comparator === '$lte') {
+            return ($actualValue <= $expectedValue);
+        } elseif ($comparator === '$gt') {
+            return ($actualValue > $expectedValue);
+        } elseif ($comparator === '$gte') {
+            return ($actualValue >= $expectedValue);
+        } else {
+            throw new DomainException('Unknown comparator "' . $comparator . '" given');
+        }
+    }
+
+    private function fetchValue($data, $column)
+    {
+        // TODO: dotted notation
+        return isset($data[$column]) ? $data[$column] : null;
+    }
+
+    private function isObject($value)
+    {
+        return (is_array($value) && ($value === array() || !isset($value[0])));
+    }
+
+    private function isVector($value)
+    {
+        return ($value === array() || (is_array($value) && isset($value[0])));
+    }
+}
